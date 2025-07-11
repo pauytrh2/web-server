d96ac4af4c26f02ce74019cf70d7b8f544270c4d
pauytrh2 <pauytrh@gmail.com>
2025-07-11 16:40:33 +0300

    Made DEFAULT_SITE a static instead of hard-coded

--- src/verify_site/mod.rs
+++ src/verify_site/mod.rs
@@ -4,6 +4,15 @@ use std::{
     path::Path,
 };
 
+const DEFAULT_SITE: &[u8] = b"<!DOCTYPE html>
+<html>
+<head><title>Welcome</title></head>
+<body>
+<h1>Hello from HTTP Server</h1>
+<p>This is the default index.html page. You may replace it with any static site.</p>
+</body>
+</html>";
+
 pub fn handle_site(site_dir: &Path, index_path: &Path) -> Result<()> {
     if !site_dir.exists() {
         create_dir_all(site_dir)?;
@@ -14,16 +23,7 @@ pub fn handle_site(site_dir: &Path, index_path: &Path) -> Result<()> {
 
     if !index_path.exists() {
         let mut file = File::create(index_path)?;
-        file.write_all(
-            b"<!DOCTYPE html>
-<html>
-<head><title>Welcome</title></head>
-<body>
-<h1>Hello from HTTP Server</h1>
-<p>This is the default index.html page. You may replace it with any static site.</p>
-</body>
-</html>",
-        )?;
+        file.write_all(DEFAULT_SITE)?;
         println!("Created default 'site/index.html'");
     } else {
         println!("Found 'site/index.html'")
